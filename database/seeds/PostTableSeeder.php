cc8d3d426c154a35cc8ca5427ea63924cac2f3a9
Pier.W <factory666@live.it>
2019-06-02 19:25:59 +0200

    git init

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Post::class, 100) -> make()
+                ->each(function($post){
+
+                  $post -> save();
+                });
+    }
+}
