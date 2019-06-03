89da60d3a8d685aff65bea92981b7d9998539275
Pier.W <factory666@live.it>
2019-06-04 01:09:02 +0200

    point 2 completed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
     public function getPostsByCategory($type)
     {
-      dd($type);
+      // dd($type);
+      $category = Category::where('type', $type) -> first();
+
+      return view('page.category', compact('category'));
     }
 }
---
+++ resources/views/page/category.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.template')
+
+@section('content')
+<h1 id="category" >{{$category -> type}}</h1>
+<table>
+  <thead>
+    <td>Titolo</td>
+    <td>Categorie</td>
+    <td>Updated At</td>
+  </thead>
+  <tbody>
+    @foreach($category -> posts as $post)
+      <tr>
+        <td>{{ $post -> title }}</td>
+        <td>
+          {{ $post -> categories -> count()}}:<br>
+          @foreach($post -> categories as $category)
+            <a href="{{route('posts.by.category', $category -> type)}}">
+              {{$category -> type}}
+            </a><br>
+          @endforeach
+        </td>
+        <td>{{ $post -> updated_at}}</td>
+      </tr>
+    @endforeach
+  </tbody>
+</table>
+@stop
